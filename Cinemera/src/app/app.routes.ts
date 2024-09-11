b0ac5230d47e56e5f590a842457ddc4dafb858d6
Basma516 <basmaelmenshawy3@gmail.com>
2024-09-12 01:55:15 +0300

    routes

--- Cinemera/src/app/app.routes.ts
+++ Cinemera/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'movie/:id',  
+        component: MovieDetailsComponent,
+      }
+      
+];
