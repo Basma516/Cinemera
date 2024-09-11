86eae939d2fc3ebc8870fb2dbff603ea64427eb0
“callmegemy” <callmegemy@gmail.com>
2024-09-11 18:17:11 +0300

    MoviesList Component First Design with fetching movies From the Api

--- Cinemera/src/app/components/movies-list/movies-list.component.ts
+++ Cinemera/src/app/components/movies-list/movies-list.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MovieService } from '../../services/movie.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-movies-list',
   standalone: true,
-  imports: [],
-  templateUrl: './movies-list.component.html',
-  styleUrl: './movies-list.component.css'
+  imports: [
+    CommonModule
+  ],
+  templateUrl: './movies-list.component.html'
 })
-export class MoviesListComponent {
+export class MoviesListComponent implements OnInit {
+  movies: any[] = [];
+  currentPage = 1;
+
+  constructor(private movieService: MovieService) {}
+
+  ngOnInit(): void {
+    this.getMoviesByPage(this.currentPage);
+  }
+
+  getMoviesByPage(page: number): void {
+    this.movieService.getPaginatedMovies(page).subscribe((data: any) => {
+      this.movies = data.results;
+    });
+  }
+
+  nextPage(): void {
+    this.currentPage++;
+    this.getMoviesByPage(this.currentPage);
+  }
+
+  previousPage(): void {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.getMoviesByPage(this.currentPage);
+    }
+  }
+
 
 }
