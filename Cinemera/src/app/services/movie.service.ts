10e46c90433fec73aef863eabf6fd8308e20791d
rawansaied <90210323+rawansaied@users.noreply.github.com>
2024-09-11 13:19:16 +0300

    movie service

--- Cinemera/src/app/services/movie.service.ts
+++ Cinemera/src/app/services/movie.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
 
-  constructor() { }
+ 
+  private apiUrl ='https://api.themoviedb.org/3';
+  private apiKey = 'af26f0977fa2e1a5d13c417f16c0110d';
+
+  constructor(private http: HttpClient) { }
+
+  getCinemeraMovies(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/movie/now_playing?api_key=${this.apiKey}`);
+  }
+  getCinemeraMovieDetails(id: number): Observable<any> {
+    return this.http.get(`${this.apiUrl}/movie/${id}?api_key=${this.apiKey}`);
+  }
+  getCinemeraRecommendations
+  (movieId: number): Observable<any> {
+    return this.http.get(`${this.apiUrl}/movie/${movieId}/recommendations?api_key=${this.apiKey}`);
+  }
 }
